1a87b47cb34cb5f2179527c6f9a59d51bba14280
DevGameplai <102517269+DevGameplai@users.noreply.github.com>
2022-04-04 14:24:46 +0100

    Improved images, light/dark themes

--- get_attributes.js
+++ get_attributes.js
@@ -2,6 +2,14 @@ function getBetId() {
   return window.frameElement.parentElement.getAttribute('betid');
 }
 
+function getTarget() {
+  return window.frameElement.parentElement.getAttribute('target');
+}
+
 function getSport() {
   return window.frameElement.parentElement.getAttribute('sport');
+}
+
+function getTheme() {
+  return window.frameElement.parentElement.getAttribute('theme');
 }
